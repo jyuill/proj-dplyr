a9fb166b756155d0111b85a7a9c309df6191a6f5
jyuill <john@fig4.com>
2015-11-30 22:49:33 -0800

    more

--- dplyr1.R
+++ dplyr1.R
@@ -23,6 +23,7 @@ str(hflights)
 flight <- tbl_df(hflights)
 flight
 head(flight)
+summary(flight)
 
 #### dplyr
 ## FILTER rows
@@ -37,9 +38,11 @@ filter(flight, TailNum %in% c("N565AA","N576AA"))
 filter(flight, TailNum %in% "N565AA")
 # >=
 filter(flight, DepTime >=1400)
-
+filter(flight,is.na(ArrTime))
+filter(flight,!is.na(ArrTime))
 # boolean
 # ==, >, <, >=, <=,!=,%in%
+
 
 ## SELECT columns
 fcols <- select(flight,UniqueCarrier,DepDelay,Cancelled,Diverted)
@@ -66,5 +69,55 @@ head(select(flight,Year:DepTime))
 # the more logical-flowing left-right
 flight %>% select(Year:DepTime) %>% head
 
-## ARRANGE order of rows
+## ARRANGE order of rows aka 'sort'
 flight %>% arrange(FlightNum) %>% head
+# piping together select cols then arrange (sort) on multiple rows, then filter, 
+# then show head
+flight %>% 
+  select(Year,Month,DayofMonth,DepTime,FlightNum) %>%
+  arrange(FlightNum,DepTime) %>%
+  filter(DepTime>1200) %>%
+  head
+# as above but descend sort on Flightnum
+flight %>% 
+  select(Year,Month,DayofMonth,DepTime,FlightNum) %>%
+  arrange(desc(FlightNum),DepTime) %>%
+  filter(DepTime>1200) %>%
+  head
+
+## MUTATE new cols
+# add col to calc flight time (not actually accurate due to 60 min in hr)
+flight %>%
+  mutate(ftime=ArrTime-DepTime) %>%
+  select(FlightNum,DepTime,ArrTime,ftime)
+  head
+# multiple new cols at once - calc speed (demonstration, not nec. accurate)
+flight %>%
+  mutate(ftime=ArrTime-DepTime, speed=Distance/(AirTime/60)) %>%
+  select(FlightNum,DepTime,ArrTime,ftime,AirTime,Distance,speed)
+head
+
+## SUMMARISE to create summaries for a given col
+# get the mean for AirTime, removing NA first
+flight %>%
+  filter(!is.na(AirTime)) %>%
+  summarise(avgairtime=mean(AirTime))
+flight %>%
+  filter(!is.na(AirTime)) %>%
+  summarise(avgairtime=mean(AirTime),
+            maxairtime=max(AirTime),
+            minairtime=min(AirTime),
+            totalairtime=sum(AirTime),
+            totalcount=n())
+
+## GROUP BY specified dimension (as in SQL)
+flight %>%
+  filter(!is.na(AirTime)) %>%
+  group_by(UniqueCarrier) %>%
+  summarise(avgairtime=mean(AirTime),
+            maxairtime=max(AirTime),
+            minairtime=min(AirTime),
+            totalairtime=sum(AirTime),
+            totalcount=n())
+
+
