ae012bd9eca47b7dc67dde429e34ea7188356d21
jyuill <john@fig4.com>
2015-12-20 17:54:44 -0800

    broke out date into year and month columns, produced monthly boxplot of temp

--- dplyr-chicago.R
+++ dplyr-chicago.R
@@ -1,12 +1,15 @@
 ### DPLYR with Chicago Temperature Data
 ## Roger Peng lesson: https://www.youtube.com/watch?v=aywFompr1F4 
 ## data retrieved from: https://github.com/DataScienceSpecialization/courses/tree/master/03_GettingData/dplyr
+library(dplyr)
+
 chicago <- readRDS("chicago.rds")
 
 str(chicago)
 summary(chicago)
 head(chicago)
 
+library(ggplot2)
 qplot(date,tmpd,data=chicago)
 
 # some things to do with dplyr:
@@ -14,3 +17,13 @@ qplot(date,tmpd,data=chicago)
 # group_by month and summarize based on mean
 # convert temps from fahrenheit to celsius (mutate)
 # change column names to be more readable
+
+library(lubridate)
+# use lubridate to extract month and year from date
+chicagomy <- chicago %>% mutate(month=month(date), year=year(date))
+chicagomy$month <- as.factor(chicagomy$month) # convert month from numeric to factor
+# (optional) create temp table and filter out any NA values in temp
+chitemp <- select(chicagomy,tmpd,date,month,year) %>% filter(!is.na(tmpd))
+# boxplot by month
+qplot(month,tmpd,data=chitemp,geom="boxplot")
+
